312303307ba7cfaf25bb07b6c722489258b812a8
john <devnull@localhost>
2019-03-23 14:44:41 +0000

    Inline method

--- python/trivia.py
+++ python/trivia.py
@@ -23,11 +23,8 @@ class Game:
             self.pop_questions.append("Pop Question %s" % i)
             self.science_questions.append("Science Question %s" % i)
             self.sports_questions.append("Sports Question %s" % i)
-            self.rock_questions.append(self.create_rock_question(i))
-    
-    def create_rock_question(self, index):
-        return "Rock Question %s" % index
-    
+            self.rock_questions.append("Rock Question %s" % i)
+
     def add(self, player_name):
         self.players.append(player_name)
         self.places[self.how_many_players] = 0
